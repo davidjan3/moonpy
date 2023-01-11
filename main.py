7e43346c1d534f4435725ef4a9d9d2827a0cbff4
davidjan3 <david.jandrey@gmail.com>
2023-01-11 22:15:45 +0100

    Doubled profits, halfed drawdowns

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ class MACDAction(Strategy):
     # ATR
     n_atrLen = 30
     n_atrThres = 1.5
+    n_atrLong = 60
     # Bollinger Bands
     n_bbLen = 30
     n_bbScale = 2
@@ -33,7 +34,7 @@ class MACDAction(Strategy):
     # Volume Filter
     b_vfUse = False
     n_vfLong = 60*12
-    n_vfShort = 15
+    n_vfShort = 5
     # TP/SL
     n_tpThres = 1.2
     n_slThres = 0.6
@@ -52,6 +53,8 @@ class MACDAction(Strategy):
                            self.n_macdSlow, self.n_macdSignal)
         # ATR
         self.atr = self.I(ta.atr, high, low, close, self.n_atrLen)
+        self.atrLong = self.I(ta.sma, ta.atr(
+            high, low, close, self.n_atrLen*2, percent=True), 60*6)
         # Bollinger Bands
         self.bb = self.I(ut.bbands, close, self.n_bbLen,
                          self.n_bbScale, overlay=True)
@@ -72,7 +75,8 @@ class MACDAction(Strategy):
         cl_macd = macd[-1] < 0 and ut.crossover(macd, signal)
         cs_macd = macd[-1] > 0 and ut.crossunder(macd, signal)
         # ATR
-        c_atr = self.atr[-1] > abs(macd[-1]) * self.n_atrThres
+        c_atr = self.atr[-1] > abs(macd[-1]) * \
+            self.n_atrThres
         # Bollinger Bands
         bbL = self.bb[0][-1]
         bbM = self.bb[1][-1]
@@ -86,7 +90,7 @@ class MACDAction(Strategy):
         cs_tf = (not self.b_tfUse) or (
             close > self.tfLong[-1] and close < self.tfShort[-1])
         # Volume Filter
-        c_vf = (not self.b_vfUse) or self.vfShort[-1] > self.vfLong[-1] * 0.5
+        c_vf = (not self.b_vfUse) or self.vfShort[-1] > self.vfLong[-1]
         # TP/SL
         l_close = (close * (1 + commission))
         s_close = (close * (1 - commission))
@@ -94,11 +98,15 @@ class MACDAction(Strategy):
         s_tp = (s_close - bbW * self.n_tpThres)
         l_sl = (l_close - bbW * self.n_slThres)
         s_sl = (s_close + bbW * self.n_slThres)
+        # cl_bb = cl_bb and l_close < bbM and s_close > bbL
+        # cs_bb = cs_bb and s_close > bbM and l_close < bbH
+
+        amount = 1.0 * max(min(self.atrLong[-1] / 0.2, 0.999), 0.25)
 
         if cl_macd and c_atr and cl_bb and cl_tf and c_vf:
-            self.buy(size=self.n_amount, tp=l_tp, sl=l_sl)
+            self.buy(size=amount, tp=l_tp, sl=l_sl)
         elif cs_macd and c_atr and cs_bb and cs_tf and c_vf:
-            self.sell(size=self.n_amount, tp=s_tp, sl=s_sl)
+            self.sell(size=amount, tp=s_tp, sl=s_sl)
 
 
 btc = pd.read_csv("./data/mBTC.csv")
